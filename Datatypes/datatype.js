f544ba5c6c63fb5f91185c1ef737e6f1bc15ab36
Ankita Nag <ankeetaa3@gmail.com>
2025-02-26 13:46:20 +0530

    Added datatypes file

---
+++ Datatypes/datatype.js
@@ -0,0 +1,16 @@
+"use strict" //treat all JS code as newer version 
+
+let name="Ankita";
+let age=20;
+let isLoggedIn=false;
+//PRIMITIVE
+ //number=>2^53
+ //bigint
+ //string=>""
+ //boolean=>true/false
+ //null => stand alone value different from undefined (empty) type is object
+ //undefined=> not defined (value still not defined) type is undefined
+ console.table([typeof name,typeof age,typeof isLoggedIn,typeof null,typeof undefined]);
+ //symbol => unique
+  //NON_PRIMITIVE
+  //object
