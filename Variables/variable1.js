e16ff0548d0e91798e253ea3d28a15e9030bd98d
Ankita Nag <ankeetaa3@gmail.com>
2025-02-26 13:31:31 +0530

    Initial

---
+++ Variables/variable1.js
@@ -0,0 +1,15 @@
+const accountId = 144553; //For declaring constant
+let accountEmail="ankee2@gmail.comm";
+var accountPassword="12345";
+accountCity= "Silchar";  //not preferable
+let accountState; //if not defined javascrpit take this as undefined
+//  accountId=7; This is not allowed as it is constant
+ accountEmail="ashh@gmail.com";
+ accountPassword="Hello90";
+ accountCity="Shree";
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState]); //For printing everything together in a tabular format basically forms an array with indexes
+
+ //2 types of declaring variable
+ //1.let 2.var
+ //Earlier in Javascript,"var" has the problem of scope and thus "let" came in 
+ 
