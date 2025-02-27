403c9a93c1c7c88c6f707174ffe7544d579d310b
Ankita Nag <ankeetaa3@gmail.com>
2025-02-27 20:33:00 +0530

    number

---
+++ Datatypes/number.js
@@ -0,0 +1,15 @@
+let score=900;
+let balance = new Number(300);
+console.log(score);
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(2)); //used in ecommerce site to show price with maximum two digit precision
+const num1=965.6544
+console.log(num1.toPrecision(3)); //rounding off
+const hundreds=1000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
